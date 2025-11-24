b15f8ef522ff5fdbff5119a2290e2c88e82ae112
Zy-Arandia <laran595@mtroyal.ca>
2025-11-23 21:39:27 -0700

    created headerfile

---
+++ src/components/headerNav.jsx
@@ -0,0 +1,13 @@
+const headerNav = () => { 
+    return (
+        <nav>
+            <ul className="flex flex-row bg-red-200">
+                <li><a href="#home">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#services">Services</a></li>
+            </ul>
+        </nav>
+    )
+}
+
+export default headerNav;
