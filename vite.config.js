0634a6f131c1e141c33248c23c3ab2ad327b75dc
Zy-Arandia <laran595@mtroyal.ca>
2025-12-08 16:35:08 -0700

    render config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/COMP4513A2/',
+  base: './',
   plugins: [react(),tailwindcss()],
 })
